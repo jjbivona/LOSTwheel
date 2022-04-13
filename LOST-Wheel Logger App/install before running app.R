196ff44f51f43cc4e83368d0594951b62f4e9dff
JJ Bivona <75329186+jjbivona@users.noreply.github.com>
2022-04-13 13:35:35 -0400

    Rename LOST-Wheel Logger App/install before running app.R to [OLD] LOST-Wheel Logger R App/install before running app.R

--- LOST-Wheel Logger App/install before running app.R
+++
@@ -1,3 +0,0 @@
-
-#Click run to install all packages needed for the lost wheel logger
-install.packages(c("shiny", "serial", "tidyverse", "ggplot2", "ggpubr", "dplyr", "data.table", "tibble", "stringr", "tidyr", "shinybusy", "shinythemes", "shinyjs"), dependencies = TRUE)
